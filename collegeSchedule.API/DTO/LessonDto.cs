66a179c0178ae5972658043825893bd189561a01
Marina <ilienko.m2005@gmail.com>
2026-02-11 19:14:50 +0700

    Создание файлов в DTO: LessonPartDto, LessonDto, ScheduleByDateDto.

---
+++ collegeSchedule.API/DTO/LessonDto.cs
@@ -0,0 +1,18 @@
+﻿using collegeSchedule.API.Models;
+
+namespace collegeSchedule.API.DTO
+{
+    public class LessonDto
+    {
+        public int LessonNumber { get; set; }
+        public string Time { get; set; } = null!; 
+        public string Subject { get; set; } = null!;
+        public string Teacher { get; set; } = null!;
+        public string TeacherPosition { get; set; } = null!;
+        public string Classroom { get; set; } = null!;
+        public string Building { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        // Словарь для подгрупп (FULL, SUB1, SUB2)
+        public Dictionary<LessonGroupPart, LessonPartDto?> GroupParts { get; set; } = new();
+    }
+}
---
+++ collegeSchedule.API/DTO/ScheduleByDateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace collegeSchedule.API.DTO
+{
+    public class ScheduleByDateDto
+    {
+        public DateTime LessonDate { get; set; }
+        public string Weekday { get; set; } = null!;
+        public List<LessonDto> Lessons { get; set; } = new(); // Список пар на этот день
+    }
+}
